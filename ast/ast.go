206ae9657a276dfb57a48c2f650f5c97b4c40aee
Vincent White <xstrengthofonex@gmail.com>
2021-08-02 01:19:51 +0900

    test symbol and call

--- ast/ast.go
+++ ast/ast.go
@@ -1,2 +1,97 @@
 package ast
 
+import (
+	"fmt"
+	"strings"
+)
+
+type Expr interface {
+	Equals(Expr) bool
+	String() string
+}
+
+type Symbol struct {
+	Value string
+}
+
+func (s1 *Symbol) Equals(other Expr) bool {
+	if other == nil {
+		return false
+	} else if s2, ok := other.(*Symbol); ok {
+		return s1.Value == s2.Value
+	}
+	return false
+}
+
+func (s *Symbol) String() string {
+	return s.Value
+}
+
+type Call struct {
+	Operator Expr
+	Operands []Expr
+}
+
+func (c1 *Call) Equals(other Expr) bool {
+	if other == nil {
+		return false
+	} else if c2, ok := other.(*Call); ok {
+		if !c1.Operator.Equals(c2.Operator) {
+			return false
+		}
+
+		if len(c1.Operands) != len(c2.Operands) {
+			return false
+		}
+
+		for i := range c1.Operands {
+			if !c1.Operands[i].Equals(c2.Operands[i]) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (s *Call) String() string {
+	operands := []string{}
+	for _, operand := range s.Operands {
+		operands = append(operands, operand.String())
+	}
+	return fmt.Sprintf("(%s %s)", s.Operator, strings.Join(operands, " "))
+}
+
+
+type Fn struct {
+	Params []*Symbol
+	Body Expr
+}
+
+func (f1 *Fn) Equals(other Expr) bool {
+	if other == nil {
+		return false
+	} else if f2, ok := other.(*Fn); ok {
+		if len(f1.Params) != len(f2.Params) {
+			return false
+		}
+
+		for i := range f1.Params {
+			if !f1.Params[i].Equals(f2.Params[i]) {
+				return false
+			}
+		}
+
+		if !f1.Body.Equals(f2.Body) {
+			return false
+		}
+	}
+	return true
+}
+
+func (f *Fn) String() string {
+	params := []string{}
+	for _, param := range f.Params {
+		params = append(params, param.String())
+	}
+	return fmt.Sprintf("fn %s . %s", strings.Join(params, " "), f.Body)
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,2 +1,122 @@
 package parser
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/xstrengthofonex/chingu/ast"
+	"github.com/xstrengthofonex/chingu/scanner"
+	"github.com/xstrengthofonex/chingu/token"
+)
+
+type Parser struct {
+	scanner *scanner.Scanner
+	eof bool
+	current token.Token
+	previous token.Token
+}
+
+func New(src string) *Parser {
+	p := &Parser{scanner: scanner.New(src)}
+	p.next()
+	return p
+}
+
+func (p *Parser) next() {
+	if !p.eof {
+		tok := p.scanner.Scan()
+		if tok.Kind == token.EOF {
+			p.eof = true
+		}
+		p.previous = p.current
+		p.current = tok
+	}
+}
+
+func (p *Parser) match(kind token.Kind) bool {
+	if p.current.Kind == kind {
+		p.next()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) expect(kind token.Kind) {
+	if p.current.Kind == kind {
+		p.next()
+		return
+	}
+	panic(fmt.Errorf("expected %s, got %s", kind, p.current.Kind))
+}
+
+func (p *Parser) parseExpr() ast.Expr {
+	if p.match(token.IDENT) {
+		return &ast.Symbol{Value: p.previous.Literal}
+	} else if p.match(token.LPAREN) {
+		operator := p.parseExpr()
+		operands := []ast.Expr{}
+		for p.current.Kind != token.RPAREN && !p.eof {
+			operands = append(operands, p.parseExpr())
+		}
+		p.expect(token.RPAREN)
+		return &ast.Call{Operator: operator, Operands: operands}
+	} else if p.match(token.FN) {
+		params := []*ast.Symbol{}
+		for p.match(token.IDENT) {
+			params = append(params, &ast.Symbol{Value: p.previous.Literal})
+		}
+		p.expect(token.DOT)
+		body := p.parseExpr()
+		return &ast.Fn{Params: params, Body: body}
+	}
+	panic(fmt.Errorf("unexpected token %s", p.current))
+}
+
+
+func (p *Parser) Parse() (expr ast.Expr, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(error); ok {
+				err = e
+			} else {
+				panic(r)
+			}
+		} 
+	}()
+
+	expr = p.parseExpr()
+
+	return expr, err
+}
+
+var parseTests = []struct{
+	src string
+	want ast.Expr
+}{
+	{"foo", &ast.Symbol{Value: "foo"}},
+	{"bar", &ast.Symbol{Value: "bar"}},
+	{"(f x y)", &ast.Call{
+		Operator: &ast.Symbol{Value: "f"},
+		Operands: []ast.Expr{
+			&ast.Symbol{Value: "x"},
+			&ast.Symbol{Value: "y"}}}},
+	{"fn x . x", &ast.Fn{
+		Params: []*ast.Symbol{{Value: "x"}},
+		Body: &ast.Symbol{Value: "x"},
+	}},
+}
+
+func TestParse(t *testing.T) {
+	for _, tc := range parseTests {
+		parser := New(tc.src)
+		got, err := parser.Parse()
+		
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !tc.want.Equals(got) {
+			t.Errorf("%q: got %v, want %v", tc.src, got, tc.want)
+		}
+	}
+}
